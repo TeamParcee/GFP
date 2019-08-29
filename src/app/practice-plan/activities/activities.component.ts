b0ef0c8319e37a86f8b706c0d21ea9aac655ba37
Adiontae Grron <adiontae.gerron@boystown.org>
2019-08-28 20:06:17 -0500

    asdfadfasdfa

--- src/app/practice-plan/activities/activities.component.ts
+++ src/app/practice-plan/activities/activities.component.ts
@@ -26,6 +26,8 @@ export class ActivitiesComponent implements OnInit {
   currentDay;
   currentWeek;
   activities;
+  startTime;
+  date;
   async ngOnInit() {
     await this.getCurrentWeek();
   }
@@ -53,12 +55,19 @@ export class ActivitiesComponent implements OnInit {
 
   getActivities() {
     firebase.firestore().collection("/users/" + this.userService.user.uid + "/weeks/" + this.currentWeek.weekId + "/days/" + this.currentDay.id + "/activities")
-    .orderBy("order")  
-    .onSnapshot((activitySnap) => {
+      .orderBy("order")
+      .onSnapshot((activitySnap) => {
         let activities = [];
+        let time = this.startTime;;
+        let minutes = 0;
         activitySnap.forEach((activity) => {
           let a = activity.data();
-          activities.push(a)
+          a.start = this.getTimeOfEvent(time, minutes);
+          a.date = this.date;
+          activities.push(a);
+
+          time = a.time;
+          minutes = a.duration;
         })
         this.activities = activities;
       })
@@ -80,4 +89,9 @@ export class ActivitiesComponent implements OnInit {
   viewActivity(activity) {
     this.helper.openModal(ViewActivityPage, { activity: activity })
   }
+
+  getTimeOfEvent(time, minutes) {
+    return moment(time, "hh:mm a").add('minutes', minutes).format('LT')
+  }
+
 }
